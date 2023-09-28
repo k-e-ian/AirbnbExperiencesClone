f391dc5449efd61b68dd79f4ab9eed1fcf0d7b8b
ian kitembe <ianedwin@outlook.com>
2023-09-28 19:48:00 +0300

    using data from data.js array of objects

--- airbnb-experience-clone/src/App.jsx
+++ airbnb-experience-clone/src/App.jsx
@@ -1,15 +1,38 @@
 //import logo from './logo.svg';
+import profilePhoto from "./assets/katie-zaferes.png"
 import './App.css';
 import NavBar from './components/NavBar';
-// import Hero from './components/Hero';
+//import Hero from './components/Hero';
 import Card from './components/Card';
+import data_exp from './data'
 
 function App() {
+const dataExp = data_exp.map((data) => {
+    return (
+      <Card
+        key={data.id}
+        img={profilePhoto}
+        rating={data.stats.rating}
+        reviewCount={data.stats.reviewCount}
+        location={data.location}
+        title={data.title}
+        price={data.price} 
+        openSpots={data.openSpots}
+      />
+    )
+}) 
+
+console.log(dataExp)
+
   return (
     <>
       <NavBar />
+      <section classname="cards-list">
+          {dataExp}
+      </section>
       
-      <Card />
+      
+     
 
     </>
 
--- airbnb-experience-clone/src/components/Card.jsx
+++ airbnb-experience-clone/src/components/Card.jsx
@@ -1,21 +1,21 @@
 import React from "react";
-import katie_png from "../assets/katie-zaferes.png";
 import star_icon from "../assets/star.png";
 
-function Card() {
+export default function Card(props) {
     return (
+        
         <div className="card">
-            <img className="card--img" src={katie_png} alt="katie-zafares-png" />
+            {props.openSpots === 0 && <div className="card--badge">SOLD OUT</div>}
+            <img className="card--image" src={props.img} alt="avatar"/>
             <div className="card--stats">
                 <img className="card--star" src={star_icon} alt="star-icon"></img>
-                <span>5.0</span>
-                <span className="gray">(6) &#8226;</span>
-                <span className="gray">USA</span>       
+                <span>{props.rating}</span>
+                <span className="gray">{props.reviewCount} &#8226;</span>
+                <span className="gray">{props.country}</span>       
             </div>
-            <p>Life Lessons with Katie Zaferes</p>
-            <p><span className="bold">From $156</span> / person</p> 
+            <p className="card--title">{props.title}</p>
+            
+            <p className="card--price"><span className="bold">From ${props.price}</span> / person</p> 
         </div>
-    )
+    ) 
 }
-
-export default Card
