5918ac2b00db16df52fbf3d4dbaa80f5873776c2
ian kitembe <ianedwin@outlook.com>
2023-09-29 14:12:31 +0300

    props as an object

--- airbnb-experience-clone/src/App.jsx
+++ airbnb-experience-clone/src/App.jsx
@@ -1,23 +1,29 @@
 //import logo from './logo.svg';
-import profilePhoto from "./assets/katie-zaferes.png"
+import profilePhoto0 from "./assets/katie-zaferes.png"
+import profilePhoto1 from "./assets/mountain-bike.png"
+import profilePhoto2 from "./assets/wedding-photography.png"
+
 import './App.css';
 import NavBar from './components/NavBar';
-//import Hero from './components/Hero';
+import Hero from './components/Hero';
 import Card from './components/Card';
 import data_exp from './data'
 
+const coverImgToProfilePhoto = {
+  "katie-zaferes.png": profilePhoto0,
+  "mountain-bike.png": profilePhoto1,
+  "wedding-photography.png": profilePhoto2,
+};
+
 function App() {
 const dataExp = data_exp.map((data) => {
+
+    const selectedProfilePhoto = coverImgToProfilePhoto[data.coverImg]
+
     return (
       <Card
         key={data.id}
-        img={profilePhoto}
-        rating={data.stats.rating}
-        reviewCount={data.stats.reviewCount}
-        location={data.location}
-        title={data.title}
-        price={data.price} 
-        openSpots={data.openSpots}
+        data={data}
       />
     )
 }) 
@@ -25,16 +31,13 @@ const dataExp = data_exp.map((data) => {
 console.log(dataExp)
 
   return (
-    <>
+    <div>
       <NavBar />
-      <section classname="cards-list">
+      <Hero/>
+      <section className="cards-list">
           {dataExp}
       </section>
-      
-      
-     
-
-    </>
+    </div>
 
   )
 }
--- airbnb-experience-clone/src/components/Card.jsx
+++ airbnb-experience-clone/src/components/Card.jsx
@@ -1,21 +1,43 @@
 import React from "react";
 import star_icon from "../assets/star.png";
+import profilePhoto0 from "../assets/katie-zaferes.png"
+import profilePhoto1 from "../assets/mountain-bike.png"
+import profilePhoto2 from "../assets/wedding-photography.png"
+
+const coverImgToProfilePhoto = {
+    "katie-zaferes.png": profilePhoto0,
+    "mountain-bike.png": profilePhoto1,
+    "wedding-photography.png": profilePhoto2,
+  };
+
+
 
 export default function Card(props) {
+    const selectedProfilePhoto = coverImgToProfilePhoto[props.data.coverImg]
+    
+
+    console.log(props.data.coverImg)
+    let badgeText
+    if (props.data.openSpots === 0){
+        badgeText = "SOLD OUT"
+    } else if (props.data.location === "Online") {
+        badgeText = "ONLINE"
+    }
+    
     return (
         
         <div className="card">
-            {props.openSpots === 0 && <div className="card--badge">SOLD OUT</div>}
-            <img className="card--image" src={props.img} alt="avatar"/>
+            {badgeText && <div className="card--badge">{badgeText}</div>}
+            <img className="card--image" src={selectedProfilePhoto} alt="avatar"/>
             <div className="card--stats">
                 <img className="card--star" src={star_icon} alt="star-icon"></img>
-                <span>{props.rating}</span>
-                <span className="gray">{props.reviewCount} &#8226;</span>
-                <span className="gray">{props.country}</span>       
+                <span>{props.data.stats.rating}</span>
+                <span className="gray">{props.data.stats.reviewCount} &#8226;</span>
+                <span className="gray">{props.data.country}</span>       
             </div>
-            <p className="card--title">{props.title}</p>
+            <p className="card--title">{props.data.title}</p>
             
-            <p className="card--price"><span className="bold">From ${props.price}</span> / person</p> 
+            <p className="card--price"><span className="bold">From ${props.data.price}</span> / person</p> 
         </div>
     ) 
 }
